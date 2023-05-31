24eb190a6f77a05864c26d80c66920317788e661
bhushan868 <bhushukarale643@gmail.com>
2023-05-31 15:12:33 +0530

    again chnages

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
       RWOnly obj=new RWOnly();
-      obj.setName("Bhushan");
+      obj.setName("Bhushank");
       String name = obj.getName();
       System.out.println(name);
     }
